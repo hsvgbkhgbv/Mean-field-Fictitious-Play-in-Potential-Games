d7bc5e2bb91643d2af9aca7308e4b12f0aa3db9a
Jianhong Wang <jianhongwang@eduroam-int-dhcp-97-156-211.ucl.ac.uk>
2018-08-17 15:56:30 +0100

    jianhong

--- simulation3.py
+++ simulation3.py
@@ -13,29 +13,28 @@ NumOfPlayers = 60
 np.random.seed(2)
 task_indice = np.random.choice(6, NumOfPlayers)
 np.random.seed()
-
 ################################################################################
-Algorithm = ['mean_field', 'joint_actions', 'actor_critic', 'average_sample']
-rewardsHistory = [[], [], [], []]
-rewardsMean = [[], [], [], []]
-Iters = 200
-for i in range(len(Algorithm)):
-    env = Inventory(NumOfPlayers, Algorithm[i], IfSnippet=False, IfGoal=True, taskIndice=task_indice)
-    for iters in range(Iters):
-        rewards = env.__procudure__()
-        rewardsHistory[i].append(rewards)
-        rewardsMean[i].append(np.sum(rewardsHistory[i])/(iters+1))
-plt.figure(figsize=(8,6))
-plt.plot(rewardsMean[0], label='MFFP')
-plt.plot(rewardsMean[1], label='JSFP')
-plt.plot(rewardsMean[2], label='ACWFP')
-plt.plot(rewardsMean[3], label='AS')
-plt.title('The Plot of the Mean of Rewards in One Situation')
-plt.xlabel('t')
-plt.ylabel('Mean of Reward Values')
-plt.grid()
-plt.legend()
-plt.show()
+# Algorithm = ['mean_field', 'joint_actions', 'actor_critic', 'average_sample']
+# rewardsHistory = [[], [], [], []]
+# rewardsMean = [[], [], [], []]
+# Iters = 1000
+# for i in range(len(Algorithm)):
+#     env = Inventory(NumOfPlayers, Algorithm[i], IfSnippet=False, IfGoal=True, taskIndice=task_indice)
+#     for iters in range(Iters):
+#         rewards = env.__procudure__()
+#         rewardsHistory[i].append(rewards)
+#         rewardsMean[i].append(np.sum(rewardsHistory[i])/(iters+1))
+# plt.figure()
+# plt.plot(rewardsMean[0], label='MFFP')
+# plt.plot(rewardsMean[1], label='JSFP')
+# plt.plot(rewardsMean[2], label='ACWFP')
+# plt.plot(rewardsMean[3], label='AS')
+# plt.title('The Plot of the Mean of Rewards in One Situation', fontsize=14, fontweight='bold')
+# plt.xlabel('Iters')
+# plt.ylabel('Mean of Reward Values')
+# plt.grid()
+# plt.legend()
+# plt.show()
 ################################################################################
 # Algorithm = 'actor_critic'
 # # Iters = 50
@@ -91,47 +90,48 @@ plt.show()
 # ani = FuncAnimation(fig, update, frames=len(frames), interval=10)
 # ani.save(maplet[Algorithm]+'_dynamics'+str(NumOfPlayers)+'.mp4', writer=writer)
 ################################################################################
-# Algorithm = ['mean_field', 'joint_actions']
-# Iters = 10000
-# Steps = 1000
-# experiments_times = 100
-# experiments = [{'num_finish': [], 'wall_crash': [], 'conflicts': [], 'iters': []},\
-#                 {'num_finish': [], 'wall_crash': [], 'conflicts': [], 'iters': []}]
-# for i in range(len(Algorithm)):
-#     for exp in range(experiments_times):
-#         env = Inventory(NumOfPlayers, Algorithm[i], IfSnippet=False, IfGoal=True, taskIndice=task_indice)
-#         frames = []
-#         frames.append(env.layout_)
-#         print ('This is the experiment {}!'.format(exp))
-#         print(frames[-1])
-#         for step in range(Steps):
-#             print ('Step {}'.format(step))
-#             last_rewards = np.zeros((NumOfPlayers, ))
-#             env.reset_layout()
-#             for iters in range(Iters):
-#                 if iters == Iters - 1:
-#                     env.record_flag = True
-#                 rewards = env.__procudure__()
-#                 env.record_flag = False
-#                 if np.sum(np.abs(np.array(rewards) - last_rewards)) < 1e-4:
-#                     experiments['iters'].append(iters)
-#                     break
-#                 last_rewards = np.array(rewards)
-#             env.update_layout()
-#             frames.append(env.layout_)
-#             env.update_start_states(env.states)
-#             num_finish = sum(env.check_assignments())
-#             print (frames[-1])
-#             print ('The number of successful assignments is {}.'.format(num_finish))
-#             print ('The number of wall crash is {}.'.format(env.wall_crash))
-#             print ('The number of conflicts is {}.\n'.format(env.conflicts))
-#             if num_finish == 60:
-#                 experiments[i]['num_finish'].append(step)
-#                 experiments[i]['wall_crash'].append(env.wall_crash)
-#                 experiments[i]['conflicts'].append(env.conflicts)
-#                 break
-# np.save('./mffp_exp3.npy', np.array([experiments[0]['num_finish'], experiments[0]['wall_crash'], experiments[0]['conflicts'], experiments[0]['iters']]))
-# np.save('./jsfp_exp3.npy', np.array([experiments[1]['num_finish'], experiments[1]['wall_crash'], experiments[1]['conflicts'], experiments[1]['iters']]))
+Algorithm = ['mean_field', 'joint_actions']
+Iters = 10000
+Steps = 1000
+experiments_times = 100
+experiments = [{'num_finish': [], 'wall_crash': [], 'conflicts': [], 'iters': [], 'rewards': []},\
+                {'num_finish': [], 'wall_crash': [], 'conflicts': [], 'iters': [], 'rewards': []}]
+for i in range(len(Algorithm)):
+    for exp in range(experiments_times):
+        env = Inventory(NumOfPlayers, Algorithm[i], IfSnippet=False, IfGoal=True, taskIndice=task_indice)
+        frames = []
+        frames.append(env.layout_)
+        print ('This is the experiment {}!'.format(exp))
+        print(frames[-1])
+        for step in range(Steps):
+            print ('Step {}'.format(step))
+            last_rewards = np.zeros((NumOfPlayers, ))
+            env.reset_layout()
+            for iters in range(Iters):
+                if iters == Iters - 1:
+                    env.record_flag = True
+                rewards = env.__procudure__()
+                env.record_flag = False
+                if np.sum(np.abs(np.array(rewards) - last_rewards)) < 1e-4:
+                    experiments['iters'].append(iters)
+                    experiments['rewards'].append(np.sum(rewards))
+                    break
+                last_rewards = np.array(rewards)
+            env.update_layout()
+            frames.append(env.layout_)
+            env.update_start_states(env.states)
+            num_finish = sum(env.check_assignments())
+            print (frames[-1])
+            print ('The number of successful assignments is {}.'.format(num_finish))
+            print ('The number of wall crash is {}.'.format(env.wall_crash))
+            print ('The number of conflicts is {}.\n'.format(env.conflicts))
+            if num_finish == 60:
+                experiments[i]['num_finish'].append(step)
+                experiments[i]['wall_crash'].append(env.wall_crash)
+                experiments[i]['conflicts'].append(env.conflicts)
+                break
+np.save('./mffp_exp3.npy', np.array([experiments[0]['num_finish'], experiments[0]['wall_crash'], experiments[0]['conflicts'], experiments[0]['iters'], experiments[0]['rewards']]))
+np.save('./jsfp_exp3.npy', np.array([experiments[1]['num_finish'], experiments[1]['wall_crash'], experiments[1]['conflicts'], experiments[1]['iters'], experiments[1]['rewards']]))
 ################################################################################
 # for name in experiments[0].keys():
 #     twosample_results = ttest_ind(experiments[0][name], experiments[1][name])
