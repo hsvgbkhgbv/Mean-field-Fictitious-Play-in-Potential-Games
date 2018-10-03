61a33441cdf42d93ade0b6a08ef9041c0bbd67e9
Jianhong Wang <jianhongwang@dyn896-236.eduroam.ic.ac.uk>
2018-10-03 11:29:51 +0100

    jianhong

--- agent.py
+++ agent.py
@@ -34,7 +34,7 @@ class Agent:
             self.action = 0
             self.enermy_action_hist = np.zeros((NumActions, ), dtype=np.float32)
             self.c = .1
-            self.d = .03
+            self.d = .001
         elif self.type == 'sample_average':
             self.Qj = np.random.rand(NumActions)
             # self.action = np.random.choice(NumActions)
